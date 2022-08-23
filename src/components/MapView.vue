17828292b5280e5b69cdcf94f11b5fcc13eaa113
GoodLuc <lucas@ombustudio.com.ar>
2022-08-23 19:23:50 -0300

    UI selector over map.

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,120 +1,161 @@
 <script setup>
-import { onMounted, ref, defineProps } from "vue";
-import Map from "@arcgis/core/Map";
-import SceneView from "@arcgis/core/views/SceneView";
-import esriConfig from "@arcgis/core/config";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import LineSymbol3D from "@arcgis/core/symbols/LineSymbol3D";
-import PathSymbol3DLayer from "@arcgis/core/symbols/PathSymbol3DLayer";
-import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import { onMounted, ref, defineProps, watch } from "vue"
+import Map from "@arcgis/core/Map"
+import SceneView from "@arcgis/core/views/SceneView"
+import esriConfig from "@arcgis/core/config"
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
+import LineSymbol3D from "@arcgis/core/symbols/LineSymbol3D"
+import PathSymbol3DLayer from "@arcgis/core/symbols/PathSymbol3DLayer"
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer"
 
 const props = defineProps({
-  dataField: {
-    type: String,
-    required: false,
-    default: "area_p_19",
-  },
-  mapStyle: {
-    type: String,
-    required: false,
-    default: "arcgis-dark-gray",
-  },
-});
+	dataField: {
+		type: String,
+		required: false,
+		default: "area_p_19",
+	},
+	mapStyle: {
+		type: String,
+		required: false,
+		default: "arcgis-dark-gray",
+	},
+})
+
+esriConfig.apiKey =
+	"AAPK874f9a5243c44c5f901ef20d4fb33b78IwDIbNkuHGvnYIUtc9i85A1lyJ-xbxoeRGHzwwsCvc_6VJPdIPSQfQLfwKhlxB-t"
 
 onMounted(() => {
-  esriConfig.apiKey = "AAPK874f9a5243c44c5f901ef20d4fb33b78IwDIbNkuHGvnYIUtc9i85A1lyJ-xbxoeRGHzwwsCvc_6VJPdIPSQfQLfwKhlxB-t";
+	const map = new Map({
+		basemap: props.mapStyle, //Basemap layer service
+		ground: "world-elevation",
+	})
 
-  const map = new Map({
-    basemap: props.mapStyle, //Basemap layer service
-    ground: "world-elevation",
-  });
+	const view = new SceneView({
+		map: map,
+		//center: [-74.006, 40.7128], // Longitude, latitude
+		//zoom: 23, // Zoom level
+		camera: {
+			position: {
+				x: -73.92999959392118,
+				y: 40.747037420327885,
+				z: 2000, // meters
+			},
+			tilt: 45,
+		},
 
-  const view = new SceneView({
-    map: map,
-    //center: [-74.006, 40.7128], // Longitude, latitude
-    //zoom: 23, // Zoom level
-    camera: {
-      position: {
-        x: -73.92999959392118,
-        y: 40.747037420327885,
-        z: 2000, // meters
-      },
-      tilt: 45,
-    },
+		qualityProfile: "high",
+		environment: {
+			lighting: {
+				directShadowsEnabled: true,
+				ambientOcclusionEnabled: true,
+			},
+			atmosphere: {
+				quality: "high",
+			},
+		},
 
-    qualityProfile: "high",
-    environment: {
-      lighting: {
-        directShadowsEnabled: true,
-        ambientOcclusionEnabled: true,
-      },
-      atmosphere: {
-        quality: "high",
-      },
-    },
+		container: "mapContainer", // Div element
+	})
 
-    container: "mapContainer", // Div element
-  });
+	const trailsRenderer = new SimpleRenderer({
+		symbol: new LineSymbol3D({
+			symbolLayers: [
+				new PathSymbol3DLayer({
+					profile: "quad",
+					material: {
+						color: [46, 255, 238],
+					},
+					width: 5,
+					height: 7,
+					join: "miter",
+					cap: "round",
+					anchor: "bottom",
+					profileRotation: "all",
+				}),
+			],
+		}),
 
-  const trailsRenderer = new SimpleRenderer({
-    symbol: new LineSymbol3D({
-      symbolLayers: [
-        new PathSymbol3DLayer({
-          profile: "quad",
-          material: {
-            color: [46, 255, 238],
-          },
-          width: 5,
-          height: 7,
-          join: "miter",
-          cap: "round",
-          anchor: "bottom",
-          profileRotation: "all",
-        }),
-      ],
-    }),
+		visualVariables: [
+			{
+				type: "color",
+				field: props.dataField,
+				stops: [
+					{ value: 50, color: "#eb4034" },
+					{ value: 2000, color: "#34eb71" },
+				],
+				legendOptions: {
+					title: "Available sidewalk area",
+				},
+			},
+		],
+	})
 
-    visualVariables: [
-      {
-        type: "color",
-        field: props.dataField,
-        stops: [
-          { value: 50, color: "#eb4034" },
-          { value: 2000, color: "#34eb71" },
-        ],
-        legendOptions: {
-          title: "Available sidewalk area",
-        },
-      },
-    ],
-  });
+	const transitLayer = new FeatureLayer({
+		url: "https://services2.arcgis.com/jncy3eo3WNLeJkJB/arcgis/rest/services/modnyc_covid_sidewalk_density_wgs84/FeatureServer/0",
+		//url: "https://services2.arcgis.com/jncy3eo3WNLeJkJB/arcgis/rest/services/nyc_covid_sidewalk_density_wgs84/FeatureServer/0",
+		opacity: 0.75,
+		renderer: trailsRenderer,
+		elevation: 10,
+	})
+	map.add(transitLayer, 0)
 
-  const transitLayer = new FeatureLayer({
-    url: "https://services2.arcgis.com/jncy3eo3WNLeJkJB/arcgis/rest/services/modnyc_covid_sidewalk_density_wgs84/FeatureServer/0",
-    //url: "https://services2.arcgis.com/jncy3eo3WNLeJkJB/arcgis/rest/services/nyc_covid_sidewalk_density_wgs84/FeatureServer/0",
-    opacity: 0.75,
-    renderer: trailsRenderer,
-    elevation: 10,
-  });
-  map.add(transitLayer, 0);
-});
+	transitLayer.when().then(() => {
+		const renderer = transitLayer.renderer.clone()
+		renderer.visualVariables = [
+			{
+				type: "color",
+				field: props.dataField,
+				stops: [
+					{ value: 50, color: "#eb4034" },
+					{ value: 2000, color: "#34eb71" },
+				],
+				legendOptions: {
+					title: "Available sidewalk area",
+				},
+			},
+		]
+		transitLayer.renderer = renderer
+	})
+
+	watch(
+		() => props.dataField,
+		() => {
+			console.log("props changed")
+			const renderer = transitLayer.renderer.clone()
+			renderer.visualVariables = [
+				{
+					type: "color",
+					field: props.dataField,
+					stops: [
+						{ value: 50, color: "#eb4034" },
+						{ value: 2000, color: "#34eb71" },
+					],
+					legendOptions: {
+						title: "Available sidewalk area",
+					},
+				},
+			]
+			transitLayer.renderer = renderer
+		}
+	)
+})
 </script>
 
 <template>
-  <div class="home">
-    <div id="mapContainer" ref="mapContainer"></div>
-  </div>
+	<div class="home">
+		<div id="mapContainer" ref="mapContainer"></div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 @import "@arcgis/core/assets/esri/themes/dark/main.css";
 body {
-  height: 100vh;
-  width: 100%;
+	height: 100vh;
+	width: 100%;
 }
 #mapContainer {
-  width: 100%;
-  height: 100%;
-  min-height: 100vh;
+	width: 100%;
+	height: 100%;
+	min-height: 100vh;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,62 @@
 <script setup>
-import MapView from "../components/MapView.vue";
+import { ref } from "vue"
+import MapView from "../components/MapView.vue"
+let currentView = ref("area_p_19")
+const dataViews = {
+	area_p_19: { label: "Sidewalk area at 7pm" },
+	area_p_12: { label: "Sidewalk area at 12pm" },
+}
 </script>
 
 <template>
-  <el-container>
-    <el-header class="main-header"><h1>Permit Request for Filming in Public Spaces</h1></el-header>
-    <el-main>
-      <article>
-        <h2>State resolution regarding the use of public space for film production activities</h2>
-        <p>When submiting your Permit Request, available sidewalk area must be taken into account.</p>
-        <p></p>
-      </article>
-    </el-main>
-  </el-container>
-  <el-button type="primary">Primary</el-button>
-  <!--  <MapView /> -->
+	<el-container>
+		<el-header class="main-header"
+			><h1>Permit Request for Filming in Public Spaces</h1></el-header
+		>
+		<el-main class="main-content">
+			<article class="selector">
+				<h2>
+					State resolution regarding the use of public space for film production
+					activities
+				</h2>
+				<p>
+					When submiting your Permit Request, available sidewalk area must be
+					taken into account.
+				</p>
+				<p></p>
+				<p>Currently displaying: {{ dataViews[currentView].label }}</p>
+				<el-button @click="currentView = 'area_p_12'" type="primary"
+					>See area_p_12</el-button
+				>
+			</article>
+			<MapView :dataField="currentView" />
+		</el-main>
+	</el-container>
 </template>
 
 <style lang="scss" scoped>
 body {
-  height: 100vh;
-  width: 100%;
+	height: 100vh;
+	width: 100%;
 }
 .main-header {
-  background: #80ed99;
-  padding: 1rem;
-  border-radius: 0.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	background: #80ed99;
+	padding: 1rem;
+	border-radius: 0.5rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.main-content {
+	position: relative;
+	.selector {
+		position: absolute;
+		top: 2rem;
+		left: 2rem;
+		width: 30%;
+		z-index: 9;
+		background: white;
+		border-radius: 0.5rem;
+	}
 }
 </style>
